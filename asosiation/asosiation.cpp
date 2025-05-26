f4cb740d86c88f6fa7a94e98185ebce75dcaf984
ridwanhasrisetiawan <ridwanhasrisetiawan@gmail.com>
2025-05-26 11:49:08 +0700

    membuat class pasien

---
+++ asosiation/asosiation.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class dokter;
+class pasien {
+    public:
+    string nama;
+    vector<dokter*> daftar_dokter;
+    pasien(string pNama) :nama(pNama) {
+        cout << "pasien \"" << nama << "\" ada\n";
+    }
+    ~pasien() {
+        cout <<"pasien \"" << nama << "\" tidak ada\n";
+    }
+    void tambahDokter(dokter*);
+    void cetakDokter();
+};
