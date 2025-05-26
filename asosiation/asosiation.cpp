110fb149f465d2ea53e15905b762bc05fdbd7d62
ridwanhasrisetiawan <ridwanhasrisetiawan@gmail.com>
2025-05-26 15:35:05 +0700

    membuat class dokter

--- asosiation/asosiation.cpp
+++ asosiation/asosiation.cpp
@@ -16,3 +16,41 @@ class pasien {
     void tambahDokter(dokter*);
     void cetakDokter();
 };
+
+class dokter {
+    public:
+    string nama;
+    vector<pasien*> daftar_pasien;
+
+    dokter(string pNama) :nama(pNama) {
+        cout << "Dokter \"" << nama << "\" ada\n";
+    }
+    ~dokter() {
+        cout << "Dokter \"" << nama << "\" tidak ada\n";
+    }
+
+    void tambahPasien(pasien*);
+    void cetakPasien();
+};
+
+void pasien::tambahDokter(dokter* pDokter) {
+    daftar_dokter.push_back(pDokter);
+}
+void pasien::cetakDokter() {
+cout << "DaftarDokter yang menangani pasien \"" <<this->nama << "\":\n";
+for (auto& a : daftar_dokter) {
+    cout << a->nama << "\n";
+}
+cout << endl;
+}
+void dokter::tambahPasien(pasien* pPasien) {
+    daftar_pasien.push_back(pPasien);
+    pPasien->tambahDokter(this);
+}
+void dokter::cetakPasien() {
+    cout << "Daftar Pasien dari dokter \"" << this->nama << "\":\n";
+    for (auto& a : daftar_pasien) {
+        cout << a->nama << "\n";
+    }
+    cout << endl;
+}
